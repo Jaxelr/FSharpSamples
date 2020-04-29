ced1c1446e7e1bec2c9a93255350b80af0531131
Jaxel Rojas <jaxelr@gmail.com>
2020-04-29 19:41:21 -0400

    feat: Added CsvHelper sample

---
+++ CsvHelper/Program.fs
@@ -0,0 +1,33 @@
+﻿open FSharp.Data
+open Insight.Database
+open System.Data.SqlClient
+
+let data = new CsvProvider<"uber.csv", HasHeaders=false, IgnoreErrors=true, Schema="MedicaidId (string), Npi (string)">()
+
+[<Literal>]
+let ConnectionString = "Server=psm-srv-12;database=mmis;Trusted_Connection=true;MultipleActiveResultSets=true;"
+
+[<EntryPoint>]
+let main argv =
+    
+    let connection = new SqlConnection(ConnectionString)
+    SqlInsightDbProvider.RegisterProvider()
+
+    let deleted = connection.ExecuteSql("DELETE FROM internal.[key]")
+
+    for row in data.Rows do
+        if (row.MedicaidId <> "999999999" && row.Npi <> "") then
+            
+            let parameters = 
+                {| 
+                    Npi = row.Npi 
+                    MedicaidId = row.MedicaidId 
+                |}
+
+            let response = connection.ExecuteSql("IF NOT EXISTS(SELECT 1 FROM internal.[key] WHERE Npi = @Npi) 
+                                                    BEGIN
+	                                                INSERT INTO internal.[Key](MedicaidId, Npi) SELECT @MedicaidId, @Npi
+                                                    END", 
+                                                parameters)
+            printfn "Inserted %i record" response
+    0 // return an integer exit code
